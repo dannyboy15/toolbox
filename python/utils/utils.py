7c206655fa17590bc0884c6027474c43eb8d4144
Daniel Bravo <bravod60@gmail.com>
2020-09-14 00:17:54 -0700

    feat(utils): Add function to get the file path of calling function/script

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+import inspect
+import os
 
 
 def md5_hash(string):
@@ -12,3 +14,17 @@ def md5_hash(string):
             The hashed string.
     """
     return hashlib.md5(string.encode("utf-8")).hexdigest()
+
+
+def get_my_dirpath():
+    """Return the /full/path/to/calling/script"""
+    # based on https://stackoverflow.com/a/55469882
+    # get the caller's stack frame and extract its file path
+    frame_info = inspect.stack()[1]
+    filepath = frame_info.filename
+    # drop the reference to the stack frame to avoid reference cycles
+    del frame_info
+
+    # make the path absolute (optional)
+    filepath = os.path.dirname(os.path.abspath(filepath))
+    return filepath
